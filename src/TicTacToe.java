7c84e2c9571648ea9fe192a342bdc4687ba18195
RAIYAN KHAN <raiyankhan2002@gmail.com>
2023-07-05 13:47:11 -0600

    new files and stuff

---
+++ src/TicTacToe.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class TicTacToe {
+
+    JFrame frame = new JFrame();
+
+    JPanel text = new JPanel();
+
+    JLabel title = new JLabel();
+
+    JPanel game = new JPanel();
+
+    TicTacToe() {
+
+
+    }
+}
